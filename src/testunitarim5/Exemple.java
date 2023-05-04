4787b5b724d11f1f3c878555de8d70781e54ca42
John Doe <johndoe@example.com>
2023-05-04 12:13:14 +0200

    Primers canvis

---
+++ src/testunitarim5/Exemple.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testunitarim5;
+
+/**
+ *
+ * @author Ruben
+ */
+public class Exemple {
+    
+    public int sumar(int num1, int num2) {
+    return num1 + num2;
+  }
+
+  public int restar(int num1, int num2) {
+    return num1 - num2;
+  }
+
+  public boolean esMajor(int edat) {
+    return edat >= 18;
+  }
+    
+}
